d285e537798f48fe32b54b3f134975ac9bd2bd49
Eugênio Moreira <eugeniojava@outlook.com>
2021-05-09 17:36:13 -0300

    front-end(feat): add basic static components

---
+++ front-end/src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          By{" "}
+          <a
+            href="https://github.com/eugeniojava"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Eugênio Moreira
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
